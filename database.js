6596182e3dce4ab8ebeaf0e2b0eea9877a378592
FarukZahiragic <fzahiragic1@etf.unsa.ba>
2025-06-01 10:29:32 +0200

    Change database name.

--- database.js
+++ database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("wt24", process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_DB_HOST,
   dialect: "mysql",
+  port:"3306",
   logging: false,
 });
 
